9928f03ef3bd7947cc40b2dcb6ae9ef6986d9838
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-09-22 22:47:54 +0300

    31 lesson - onClick,ref,VirtualDom

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,14 @@ const Dialogs = (props) => {
     let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElements = props.messages.map(m => <Message message={m.message}/>);
 
+    let newMessage = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessage.current.value;
+        alert(text);
+    }
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -15,6 +23,13 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div>
+                <textarea ref={newMessage}>Enter text</textarea>
+            </div>
+            <div>
+                <button onClick={addMessage}>Add message
+                </button>
             </div>
         </div>
     )
