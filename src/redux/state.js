8b57d792b336721e3443e5bf869e0ec8919f76a2
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-09-22 21:15:03 +0300

    29 lesson - упаковываем данные в state

---
+++ src/redux/state.js
@@ -0,0 +1,30 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 12},
+            {id: 2, message: 'It\'s my first post', likesCount: 34},
+            {id: 3, message: 'Hi, how are you?', likesCount: 20},
+            {id: 4, message: 'Hello!', likesCount: 5}
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Valera'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'I\'m ok, thank you'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Hello'},
+            {id: 6, message: 'Goodbye'}
+        ],
+    },
+}
+
+export default state;
