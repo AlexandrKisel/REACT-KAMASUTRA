0902505eeec6142d9ab4f877510309a8cc0853a5
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-10-02 21:27:33 +0300

    47 lesson - stateCopy in ProfileReducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -13,18 +13,23 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             };
-            state.posts.push(newPost);
-            state.newPostText = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newPText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state};
+            stateCopy.newPostText = action.newPText;
+            return stateCopy;
+        }
         default:
             return state;
     }
