0cb79a2f8641d9cdeee931afc7e258b9cd989126
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-10-03 22:51:33 +0300

    51 lesson - Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,21 @@
 import React from 'react';
 import s from "./Users.module.css";
+import userPhoto from '../../assets/images/userAvatar.jpg'
+import * as axios from "axios";
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {country: 'Belarus', city: 'Minsk'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://media.istockphoto.com/vectors/user-sign-icon-person-symbol-human-avatar-vector-id639085642',
-                    followed: false,
-                    fullName: 'Sasha',
-                    status: 'I am a boss too',
-                    location: {country: 'Russia', city: 'Moscow'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://icon-library.com/images/icon-avatar/icon-avatar-19.jpg',
-                    followed: true,
-                    fullName: 'Vasya',
-                    status: 'I am a boss too',
-                    location: {country: 'Ukraine', city: 'Kiev'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://w7.pngwing.com/pngs/304/305/png-transparent-man-with-formal-suit-illustration-web-development-computer-icons-avatar-business-user-profile-child-face-web-design.png',
-                    followed: false,
-                    fullName: 'Oleg',
-                    status: 'I am a boss too',
-                    location: {country: 'Poland', city: 'Warsaw'}
-                },
-            ]
-        )
+        debugger;
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        })
     }
     return <div>
         {
             props.users.map(user => <div key={user.id}>
                 <span>
                     <div>
-                        <img className={s.userPhoto} src={user.photoUrl} alt=""/>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto} className={s.userPhoto}/>
                     </div>
                     <div>
                         {user.followed
@@ -58,13 +29,13 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{user.fullName}</div>
+                        <div>{user.name}</div>
                         <div>{user.status}</div>
                     </span>
                 </span>
                 <span>
-                    <div>{user.location.country}</div>
-                    <div>{user.location.city}</div>
+                    <div>{"user.location.country"}</div>
+                    <div>{"user.location.city"}</div>
                 </span>
             </div>)
         }
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -62,9 +62,9 @@ const usersReducer = (state = initialState, action) => {
                     return user
                 })
             }
-        case SET_USERS:
-            return { ...state, users: [...state.users, ...action.users]
-            }
+        case SET_USERS: {
+            return { ...state, users: [...state.users, ...action.users]}
+        }
         default:
             return state;
     }
