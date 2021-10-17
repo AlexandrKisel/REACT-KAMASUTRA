9dfa7ccad407c74e9d3989d4647cea1cf017d9a2
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-10-17 22:40:09 +0300

    59 lesson - profilePage, ajax, api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from "./Users.module.css";
 import userPhoto from "../../assets/images/userAvatar.jpg";
+import {NavLink} from "react-router-dom";
 
 let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
-    for (let i=1; i <= pagesCount; i++) {
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
@@ -19,11 +20,14 @@ let Users = (props) => {
             })}
         </div>
         {
-           props.users.map(user => <div key={user.id}>
+            props.users.map(user => <div key={user.id}>
                 <span>
                     <div>
-                        <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto}/>
-                    </div>
+                        <NavLink to={'/profile/' + user.id}>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                             className={styles.userPhoto}/>
+                        </NavLink>
+                        </div>
                     <div>
                         {user.followed
                             ? <button onClick={() => {
