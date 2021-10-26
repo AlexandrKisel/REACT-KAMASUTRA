9e2dd9c9da182e1900470902ba95bbc05afaaf9c
AlexandrKisel <81587470+AlexandrKisel@users.noreply.github.com>
2021-10-26 21:32:58 +0300

    73 lesson - http put, local state

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,8 +1,9 @@
-import {usersAPI} from "../api/api";
+import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET-USER-PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 let initialState = {
     posts: [
@@ -12,7 +13,8 @@ let initialState = {
         {id: 4, message: 'Hello!', likesCount: 5}
     ],
     newPostText: 'it-kamasutra.com',
-    profile: null
+    profile: null,
+    status: ""
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -35,6 +37,12 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newPText
             };
         }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            };
+        }
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
@@ -45,6 +53,8 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({type: ADD_POST});
 export const setUsersProfile = (profile) => ({type: SET_USER_PROFILE, profile});
+export const setStatus = (status) => ({type: SET_STATUS, status});
+
 export const updateNewPostTextActionCreator = (text) =>
     ({type: UPDATE_NEW_POST_TEXT, newPText: text});
 
@@ -55,6 +65,23 @@ export const getUsersProfile = (userId) => {
                 dispatch(setUsersProfile(response.data));
             })
     }
+}
+
+export const getStatus = (userId) => (dispatch) => {
+        profileAPI.getStatus(userId)
+            .then(response => {
+                debugger;
+                dispatch(setStatus(response.data));
+            })
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setStatus(status));
+            }
+        })
 }
 
 export default profileReducer;
